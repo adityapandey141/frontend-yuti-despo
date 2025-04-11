dc444701beac67bdaa2ead14bc87cfa261fee173
Aditya Pandey <adityapandey141@gmail.com>
2025-04-11 17:05:10 +0530

    added form submit api with captcha api

--- src/components/contact/ContactFrom.jsx
+++ src/components/contact/ContactFrom.jsx
@@ -28,18 +28,54 @@ export default function ContactFrom() {
             <div className="flex items-center gap-4">
               <div className="bg-dg p-4 rounded">
                 <svg
+                  width="30"
+                  height="30"
+                  viewBox="0 0 48 48"
+                  id="a"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-white"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
+                  fill="#ffffff"
+                  stroke="#ffffff"
                 >
-                  <path
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M3 10h11M9 21V3m6 18v-9m6 9V3m-6 9h11"
-                  ></path>
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      class="b"
+                      d="m23.5947,15.0005c.548-1.0177,1.1956-1.996,1.9428-2.9205.7344-.9086.6287-2.236-.1974-3.0621l-3.832-3.832c-.9854-.9854-2.5956-.8934-3.4904.1749-9.0143,10.7619-9.0143,26.515,0,37.2768.8948,1.0683,2.5022,1.163,3.4876.1777l3.4162-3.4162c1.2472-1.2472,1.3503-2.5721.616-3.4807-.7472-.9245-1.3948-1.9027-1.9428-2.9205-.6703-1.2448-1.9774-2.0111-3.3913-2.0111h-3.2796c-1.3552-4.5526-1.3552-9.4226,0-13.9752h3.2796c1.4138,0,2.7209-.7663,3.3913-2.0111Z"
+                    ></path>
+                    <g>
+                      <g>
+                        <polyline
+                          class="b"
+                          points="26.3775 24.6979 23.498 27.5607 26.3775 30.4236"
+                        ></polyline>
+                        <line
+                          class="b"
+                          x1="23.498"
+                          y1="27.5607"
+                          x2="36.7429"
+                          y2="27.5607"
+                        ></line>
+                      </g>
+                      <g>
+                        <polyline
+                          class="b"
+                          points="33.8634 22.528 36.7429 19.6652 33.8634 16.8024"
+                        ></polyline>
+                        <line
+                          class="b"
+                          x1="36.7429"
+                          y1="19.6652"
+                          x2="23.498"
+                          y2="19.6652"
+                        ></line>
+                      </g>
+                    </g>
+                  </g>
                 </svg>
               </div>
               <div>
@@ -52,18 +88,22 @@ export default function ContactFrom() {
             <div className="flex items-center gap-4">
               <div className="bg-dg p-4 rounded">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-white"
-                  fill="none"
+                  width="30"
+                  height="30"
+                  fill="#ffffff"
                   viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="#ffffff"
                 >
-                  <path
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M16 12H8m0 0V8m0 4h8m-8 0v4"
-                  ></path>
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path d="M12,2 C17.4292399,2 21.8479317,6.32667079 21.9961582,11.7200952 L22,12 L22,13 C22,15.1729208 20.477434,17 18.5,17 C17.3269391,17 16.3139529,16.3570244 15.6839382,15.3803024 C14.770593,16.3757823 13.4581934,17 12,17 C9.23857625,17 7,14.7614237 7,12 C7,9.23857625 9.23857625,7 12,7 C14.6887547,7 16.8818181,9.12230671 16.9953805,11.7831104 L17,12 L17,13 C17,14.1407877 17.7160103,15 18.5,15 C19.2447902,15 19.928229,14.2245609 19.9947109,13.1689341 L20,13 L20,12 C20,7.581722 16.418278,4 12,4 C7.581722,4 4,7.581722 4,12 C4,16.418278 7.581722,20 12,20 C13.1630948,20 14.2892822,19.7522618 15.3225159,19.2798331 C15.8247876,19.0501777 16.4181317,19.271177 16.647787,19.7734487 C16.8774423,20.2757205 16.656443,20.8690646 16.1541713,21.0987199 C14.861218,21.689901 13.4515463,22 12,22 C6.4771525,22 2,17.5228475 2,12 C2,6.4771525 6.4771525,2 12,2 Z M12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 Z"></path>{" "}
+                  </g>
                 </svg>
               </div>
               <div>
@@ -76,18 +116,23 @@ export default function ContactFrom() {
             <div className="flex items-center gap-4">
               <div className="bg-dg p-4 rounded">
                 <svg
+                  width="30"
+                  height="30"
+                  fill="#ffffff"
+                  viewBox="0 0 32 32"
+                  version="1.1"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-white"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
+                  stroke="#ffffff"
                 >
-                  <path
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z"
-                  ></path>
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path d="M16.114-0.011c-6.559 0-12.114 5.587-12.114 12.204 0 6.93 6.439 14.017 10.77 18.998 0.017 0.020 0.717 0.797 1.579 0.797h0.076c0.863 0 1.558-0.777 1.575-0.797 4.064-4.672 10-12.377 10-18.998 0-6.618-4.333-12.204-11.886-12.204zM16.515 29.849c-0.035 0.035-0.086 0.074-0.131 0.107-0.046-0.032-0.096-0.072-0.133-0.107l-0.523-0.602c-4.106-4.71-9.729-11.161-9.729-17.055 0-5.532 4.632-10.205 10.114-10.205 6.829 0 9.886 5.125 9.886 10.205 0 4.474-3.192 10.416-9.485 17.657zM16.035 6.044c-3.313 0-6 2.686-6 6s2.687 6 6 6 6-2.687 6-6-2.686-6-6-6zM16.035 16.044c-2.206 0-4.046-1.838-4.046-4.044s1.794-4 4-4c2.207 0 4 1.794 4 4 0.001 2.206-1.747 4.044-3.954 4.044z"></path>{" "}
+                  </g>
                 </svg>
               </div>
               <div>
--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function Form() {
+  const [errors, setErrors] = useState({});
+
+  const [finalAlert, setFinalAlert] = useState({ message: "", type: "" });
+
+  useEffect(() => {
+    captcha();
+  }, []);
+
+  const [captchaCode, setCaptcha] = useState({
+    captchaQuestion: "",
+    captchaAnswer: "",
+  });
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     phone: "",
     subject: "",
     message: "",
+    captchaAnswer: "",
   });
 
   const handleOnchange = (e) => {
@@ -17,13 +30,98 @@ export default function Form() {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = (e) => {
+  // Configuration constants - keep these in server environment variables in production
+  const API_KEY = "3yphJDfJlWCY75GeKtY0g9GyzywQNFMB";
+  const API_ENDPOINT = "https://leads.egnioldigital.com/api/lead";
+  const CAPTCHA_ENDPOINT = "https://leads.egnioldigital.com/api/captcha";
+
+  async function submitLead() {
+    try {
+      const payload = {
+        ...formData,
+        domain: "frontend-yuti-despo.vercel.app",
+      };
+      const response = await fetch(API_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": API_KEY,
+          Origin: typeof window !== "undefined" ? window.location.origin : "",
+        },
+        credentials: "include",
+        body: JSON.stringify(payload),
+      });
+      //console.log(response.status);
+      if (!response.ok) {
+        const errorData = await response.json();
+        //console.error("API Error:", errorData);
+        return {
+          success: false,
+          error: errorData.error || "Something went wrong",
+        };
+      }
+
+      const data = await response.json();
+      console.log("Lead submitted successfully:", data);
+      return { success: true, message: "Thank you ! We will get back soon !" };
+    } catch (error) {
+      console.error("Network Error:", error);
+      return { success: false, error: error.message || "Network error" };
+    }
+  }
+
+  async function captcha() {
+    try {
+      setFormData((prev) => ({ ...prev, captchaAnswer: "" }));
+      const response = await fetch(CAPTCHA_ENDPOINT, {
+        cache: "no-store",
+        credentials: "include",
+        headers: {
+          "x-api-key": API_KEY,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to load CAPTCHA");
+      }
+
+      const data = await response.json();
+      setCaptcha({ captchaQuestion: data.question });
+      console.log(data.question);
+    } catch (error) {
+      console.error("Error loading CAPTCHA:", error);
+      // setStatus({
+      //   loading: false,
+      //   message: "Unable to load CAPTCHA. Please refresh the page.",
+      //   error: true,
+      // });
+    }
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const validationErrors = validate();
     if (Object.keys(validationErrors).length > 0) {
-      console.log(validationErrors);
+      setErrors(validationErrors);
+      console.log("errors state --", errors);
     } else {
-      console.log(formData);
+      const submitdata = await submitLead();
+      if (submitdata.success) {
+        setFinalAlert({ message: submitdata.message, type: "success" });
+        setFormData({
+          name: "",
+          email: "",
+          phone: "",
+          subject: "",
+          message: "",
+          captchaAnswer: "",
+        });
+        // console.log("Lead Submitted !", submitdata);
+      } else {
+        setFinalAlert({ message: submitdata.error, type: "error" });
+        captcha();
+        //console.log("Lead submission failed !", submitdata);
+      }
     }
   };
 
@@ -47,44 +145,98 @@ export default function Form() {
     <>
       <form className="space-y-6" onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <input
-            type="text"
-            name="name"
-            placeholder="Your name"
-            onChange={handleOnchange}
-            className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email address"
-            onChange={handleOnchange}
-            className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
-          />
+          <div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              onChange={handleOnchange}
+              value={formData.name}
+              className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
+            />
+            {errors.name && (
+              <p className="text-red text-sm mt-1">*{errors.name}</p>
+            )}
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              placeholder="Email address"
+              onChange={handleOnchange}
+              className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
+            />
+            {errors.email && (
+              <p className="text-red text-sm mt-1">*{errors.email}</p>
+            )}
+          </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <input
-            type="text"
-            name="phone"
-            onChange={handleOnchange}
-            placeholder="Phone number"
-            className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
-          />
-          <input
-            type="text"
-            placeholder="Subject"
-            name="subject"
-            onChange={handleOnchange}
-            className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
-          />
+          <div>
+            <input
+              type="text"
+              name="phone"
+              onChange={handleOnchange}
+              value={formData.phone}
+              placeholder="Phone number"
+              className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
+            />
+            {errors.phone && (
+              <p className="text-red text-sm mt-1">*{errors.phone}</p>
+            )}
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Subject"
+              name="subject"
+              value={formData.subject}
+              onChange={handleOnchange}
+              className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
+            />
+            {errors.subject && (
+              <p className="text-red text-sm mt-1">*{errors.subject}</p>
+            )}
+          </div>
         </div>
-        <textarea
-          placeholder="Write message"
-          name="message"
-          onChange={handleOnchange}
-          rows="5"
-          className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
-        ></textarea>
+        <div>
+          <textarea
+            placeholder="Write message"
+            name="message"
+            onChange={handleOnchange}
+            value={formData.message}
+            rows="5"
+            className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
+          ></textarea>
+          {errors.message && (
+            <p className="text-red text-sm mt-1">*{errors.message}</p>
+          )}
+        </div>
+
+        {/* Captcha Section */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 items-center">
+          <div className="md:col-span-2">
+            <input
+              type="text"
+              placeholder={captchaCode.captchaQuestion}
+              name="captchaAnswer"
+              value={formData.captchaAnswer}
+              onChange={handleOnchange}
+              className="w-full p-4 border rounded bg-gray-100 focus:outline-none focus:ring-0 focus:ring-dg"
+            />
+            {errors.captcha && (
+              <p className="text-red text-sm mt-1">*{errors.captcha}</p>
+            )}
+          </div>
+          <button
+            type="button"
+            onClick={captcha}
+            className=" py-3 px-4 w-full md:w-auto rounded bg-gray-300 text-black font-semibold hover:bg-gray-400 transition"
+          >
+            Refresh Captcha
+          </button>
+        </div>
 
         <button
           type="submit"
@@ -92,6 +244,32 @@ export default function Form() {
         >
           Send a Message
         </button>
+
+        {finalAlert.message && (
+          <div
+            className={`flex p-4 rounded text-white flex items-center justify-between ${
+              finalAlert.type === "success" ? "bg-dg" : "bg-red"
+            }`}
+          >
+            <div>
+              <span>{finalAlert.message}</span>
+            </div>
+            <button onClick={() => setFinalAlert({ message: "", type: "" })}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-white"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        )}
       </form>
     </>
   );
